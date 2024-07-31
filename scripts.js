2378acb83664d9b68927b791ed8312e3db3d3bfe
MikaDev <camilo.ederson@gmail.com>
2024-07-31 16:38:31 -0600

    sobre mi terminado

--- scripts.js
+++ scripts.js
@@ -5,15 +5,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const closeIcon = document.getElementById('close-icon');
 
     menuToggle.addEventListener('click', function () {
-        if (mainNav.style.display === 'flex') {
-            mainNav.style.display = 'none';
+        mainNav.classList.toggle('active'); // Alternar la clase active
+        menuIcon.style.display = mainNav.classList.contains('active') ? 'none' : 'inline';
+        closeIcon.style.display = mainNav.classList.contains('active') ? 'inline' : 'none';
+    });
+
+    // Cerrar el menú cuando se hace clic en un enlace
+    document.querySelectorAll('#main-nav a').forEach(anchor => {
+        anchor.addEventListener('click', function () {
+            mainNav.classList.remove('active');
             menuIcon.style.display = 'inline';
             closeIcon.style.display = 'none';
-        } else {
-            mainNav.style.display = 'flex';
-            menuIcon.style.display = 'none';
-            closeIcon.style.display = 'inline';
-        }
+        });
     });
 
     // Smooth scrolling for navigation links
@@ -26,5 +29,3 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
-
-
